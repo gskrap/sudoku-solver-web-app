c0b0f68ad840de37f3f1ad261d0462eb1664bc61
George Skrapits <email": null>
2016-06-07 15:19:23 -0400

    add full solution button and home button to solved page

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
-  $('.solved_cell').on('click', function(event){
+  $('.solved_cell').on('click', function(){
     this.style.color = 'gray';
+  });
+
+  $('#solution-btn').on('click', function(){
+    var solved_cells = document.getElementsByClassName('solved_cell');
+    var i;
+    for (i = 0; i < solved_cells.length; i++) {
+    solved_cells[i].style.color = 'gray';
+    }
+  });
+
+  $('#home-btn').click(function() {
+  window.location='/';
   });
 });
